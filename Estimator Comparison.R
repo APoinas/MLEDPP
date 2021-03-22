517dfcdf1dc9db31b06fef50bf5cbd85973b7d3c
APoinas <arnaud.poinas@univ-lille.fr>
2021-03-22 18:43:52 +0100

    Added new programs

---
+++ Estimator Comparison.R
@@ -0,0 +1,357 @@
+library(spatstat)
+library(stats)
+
+#For parallelization
+library(foreach)
+library(doSNOW)
+library(parallel)
+
+source("MLE_DPP.R")
+
+###############################Exemples of using the function MLEDPP#################################
+#####On a rectangular window#####
+#Gauss
+rho0 = 100
+alpha0 = 0.03
+S_length = 2
+S = simulate(dppGauss(lambda=rho0, alpha=alpha0, d=2), 1, W=owin(c(0,S_length), c(0,S_length)))
+plot(S)
+
+MLEDPP(S, "Gauss", edgecorr=FALSE)
+MLEDPP(S, "Gauss", edgecorr=TRUE)
+
+#Bessel
+rho0 = 100
+alpha0 = 0.03
+S_length = 2
+S = simulate(dppBessel(lambda=rho0, alpha=alpha0, sigma=0, d=2), 1, W=owin(c(0,S_length), c(0,S_length)))
+plot(S)
+
+MLEDPP(S, "Bessel", edgecorr=TRUE)
+
+#Cauchy
+rho0 = 100
+alpha0 = 0.02
+S_length = 2
+S = simulate(dppCauchy(lambda=rho0, alpha=alpha0, nu=0.5, d=2), 1, W=owin(c(0,S_length), c(0,S_length)))
+plot(S)
+
+MLEDPP(S, "Cauchy", edgecorr=TRUE)
+
+#Whittle-Matern
+rho0 = 100
+alpha0 = 0.015
+S_length = 2
+S = simulate(dppMatern(lambda=rho0, alpha=alpha0, nu=2, d=2), 1, W=owin(c(0,S_length), c(0,S_length)))
+plot(S)
+
+MLEDPP(S, "WM", edgecorr=TRUE, sigma=2)
+
+#####On an R-shaped window#####
+rho0 = 100
+alpha0 = 0.05
+Simu = readRDS(paste("datasets/100_Gauss_", toString(alpha0*100), "e-2_letterR.dat", sep=""))
+S = Simu[[1]]
+plot(S)
+
+MLEDPP(S, "Gauss", edgecorr=FALSE)
+MLEDPP(S, "Gauss", edgecorr=TRUE)
+
+
+###############################Comparison between estimators#################################
+#####Gauss#####
+rho0 = 100
+alpha0 = 0.03
+S_length = 1
+nb_core = 7
+n = 500
+Simu = readRDS(paste("datasets/2000_Gauss_", toString(rho0), "_", toString(alpha0*100), "e-2_[0_", toString(S_length), "].dat", sep=""))
+
+#MCE with the pcf
+T_pcf = unlist(unname(mclapply(Simu[1:n], function(x) dppm(x~1, dppGauss, method="mincon", statistic="pcf", statargs=list(divisor='d'), rmin=0.01, q=1/2)$fitted$fixedpar$alpha, mc.cores=nb_core)))
+
+#MCE with K
+T_K = unlist(unname(mclapply(Simu[1:n], function(x) dppm(x~1, dppGauss, method="mincon", statistic="K", rmin=0.01, q=1/2)$fitted$fixedpar$alpha, mc.cores=nb_core)))
+
+#Setting up the progress bar
+pb = txtProgressBar(max = n, style = 3)
+progress = function(n) setTxtProgressBar(pb, n)
+opts = list(progress = progress)
+
+#MLE (Without tore correction)
+cl = makeCluster(nb_core)
+clusterExport(cl, c('pairdist', 'is.ppp', 'area', 'dppGauss'))
+registerDoSNOW(cl)
+templist = foreach(a=1:n, .combine="cbind", .options.snow = opts) %dopar% {
+  MLEDPP(Simu[[a]], "Gauss", edgecorr=FALSE)$fixedpar$alpha}
+stopCluster(cl)
+T_MLE = unname(templist[1,])
+
+#MLE (With tore correction)
+cl = makeCluster(nb_core)
+clusterExport(cl, c('pairdist', 'is.ppp', 'area', 'dppGauss'))
+registerDoSNOW(cl)
+templist = foreach(a=1:n, .combine="cbind", .options.snow = opts) %dopar% {
+  MLEDPP(Simu[[a]], "Gauss", edgecorr=TRUE)$fixedpar$alpha}
+stopCluster(cl)
+T_MLE_corr = unname(templist[1,])
+
+#Boxplot of all statistics
+boxplot(T_MLE_corr-alpha0, T_MLE-alpha0, T_pcf-alpha0, T_K-alpha0, names=c("MLE w/ edge corr.", "MLE w/o edge corr.", "MCE w/ the pcf", "MCE w/ K"))
+abline(h=0, col="red", lty=2)
+#Mean square errors
+c(10^4*mean((T_MLE_corr-alpha0)^2), 10^4*mean((T_MLE-alpha0)^2), 10^4*mean((T_pcf-alpha0)^2), 10^4*mean((T_K-alpha0)^2))
+
+#Saving data
+all_data = data.frame(MLE_not_corrected = T_MLE, MLE_corrected = T_MLE_corr , MCE_with_g = T_pcf , MCE_with_K = T_K)
+saveRDS(all_data ,paste("results/Result_Gauss_", toString(alpha0*100), "e-2_[0_", toString(S_length), "].dat", sep=""))
+
+#####Bessel#####
+rho0 = 100
+alpha0 = 0.03
+S_length = 1
+nb_core = 7
+n = 500
+Simu = readRDS(paste("datasets/2000_Bessel_",toString(rho0),"_",toString(alpha0*100),"e-2_[0_",toString(S_length),"].dat",sep=""))
+
+#MCE with the pcf
+T_pcf = unlist(unname(mclapply(Simu[1:n], function(x) dppm(x~1, dppBessel(sigma=0), method="mincon", statistic="pcf", statargs=list(divisor='d'), rmin=0.01, q=1/2)$fitted$fixedpar$alpha, mc.cores=nb_core)))
+
+#MCE with K
+T_K = unlist(unname(mclapply(Simu[1:n], function(x) dppm(x~1, dppBessel(sigma=0), method="mincon", statistic="K", rmin=0.01, q=1/2)$fitted$fixedpar$alpha, mc.cores=nb_core)))
+
+#Setting up the progress bar
+pb = txtProgressBar(max = n, style = 3)
+progress = function(n) setTxtProgressBar(pb, n)
+opts = list(progress = progress)
+
+#MLE (Without tore correction)
+cl = makeCluster(nb_core)
+clusterExport(cl, c('pairdist', 'is.ppp', 'area', 'dppBessel'))
+registerDoSNOW(cl)
+templist = foreach(a=1:n, .combine="cbind", .options.snow = opts) %dopar% {
+  MLEDPP(Simu[[a]], "Bessel", edgecorr=FALSE)$fixedpar$alpha}
+stopCluster(cl)
+T_MLE = unname(templist[1,])
+
+#MLE (With tore correction)
+cl = makeCluster(nb_core)
+clusterExport(cl, c('pairdist', 'is.ppp', 'area', 'dppBessel'))
+registerDoSNOW(cl)
+templist = foreach(a=1:n, .combine="cbind", .options.snow = opts) %dopar% {
+  MLEDPP(Simu[[a]], "Bessel", edgecorr=TRUE)$fixedpar$alpha}
+stopCluster(cl)
+T_MLE_corr = unname(templist[1,])
+
+#Boxplot of all statistics
+boxplot(T_MLE_corr-alpha0, T_MLE-alpha0, T_pcf-alpha0, T_K-alpha0, names=c("MLE w/ edge corr.", "MLE w/o edge corr.", "MCE w/ the pcf", "MCE w/ K"))
+abline(h=0, col="red", lty=2)
+#Mean square errors
+c(10^4*mean((T_MLE_corr-alpha0)^2), 10^4*mean((T_MLE-alpha0)^2), 10^4*mean((T_pcf-alpha0)^2), 10^4*mean((T_K-alpha0)^2))
+
+#Saving data
+all_data = data.frame(MLE_not_corrected = T_MLE, MLE_corrected = T_MLE_corr , MCE_with_g = T_pcf , MCE_with_K = T_K)
+saveRDS(all_data ,paste("results/Result_Bessel_", toString(alpha0*100), "e-2_[0_", toString(S_length), "].dat", sep=""))
+
+#####Cauchy#####
+rho0 = 100
+alpha0 = 0.02
+S_length = 1
+nb_core = 7
+n = 500
+Simu = readRDS(paste("datasets/500_Cauchy_",toString(rho0),"_",toString(alpha0*1000),"e-3_[0_",toString(S_length),"].dat",sep=""))
+
+#MCE with the pcf
+T_pcf = unlist(unname(mclapply(Simu[1:n], function(x) dppm(x~1, dppCauchy(nu=0.5), method="mincon", statistic="pcf", statargs=list(divisor='d'), rmin=0.01, q=1/2)$fitted$fixedpar$alpha, mc.cores=nb_core)))
+
+#MCE with K
+T_K = unlist(unname(mclapply(Simu[1:n], function(x) dppm(x~1, dppCauchy(nu=0.5), method="mincon", statistic="K", rmin=0.01, q=1/2)$fitted$fixedpar$alpha, mc.cores=nb_core)))
+
+#Setting up the progress bar
+pb = txtProgressBar(max = n, style = 3)
+progress = function(n) setTxtProgressBar(pb, n)
+opts = list(progress = progress)
+
+#MLE (Without tore correction)
+cl = makeCluster(nb_core)
+clusterExport(cl, c('pairdist', 'is.ppp', 'area', 'dppCauchy'))
+registerDoSNOW(cl)
+templist = foreach(a=1:n, .combine="cbind", .options.snow = opts) %dopar% {
+  MLEDPP(Simu[[a]], "Cauchy", edgecorr=FALSE)$fixedpar$alpha}
+stopCluster(cl)
+T_MLE = unname(templist[1,])
+
+#MLE (With tore correction)
+cl = makeCluster(nb_core)
+clusterExport(cl, c('pairdist', 'is.ppp', 'area', 'dppCauchy'))
+registerDoSNOW(cl)
+templist = foreach(a=1:n, .combine="cbind", .options.snow = opts) %dopar% {
+  MLEDPP(Simu[[a]], "Cauchy", edgecorr=TRUE)$fixedpar$alpha}
+stopCluster(cl)
+T_MLE_corr = unname(templist[1,])
+
+#Boxplot of all statistics
+boxplot(T_MLE_corr-alpha0, T_MLE-alpha0, T_pcf-alpha0, T_K-alpha0, names=c("MLE w/ edge corr.", "MLE w/o edge corr.", "MCE w/ the pcf", "MCE w/ K"))
+abline(h=0, col="red", lty=2)
+#Mean square errors
+c(10^4*mean((T_MLE_corr-alpha0)^2), 10^4*mean((T_MLE-alpha0)^2), 10^4*mean((T_pcf-alpha0)^2), 10^4*mean((T_K-alpha0)^2))
+
+#Saving data
+all_data = data.frame(MLE_not_corrected = T_MLE, MLE_corrected = T_MLE_corr , MCE_with_g = T_pcf , MCE_with_K = T_K)
+saveRDS(all_data ,paste("results/Result_Gauss_", toString(alpha0*1000), "e-3_[0_", toString(S_length), "].dat", sep=""))
+
+#####Whittle-Matérn#####
+rho0 = 100
+alpha0 = 0.015
+S_length = 1
+nb_core = 7
+n = 500
+Simu = readRDS(paste("datasets/500_Matern_sigma=2_",toString(rho0),"_",toString(alpha0*1000),"e-3_[0_",toString(S_length),"].dat",sep=""))
+
+#MCE with the pcf
+T_pcf = unlist(unname(mclapply(Simu[1:n], function(x) dppm(x~1, dppMatern(nu=2), method="mincon", statistic="pcf", statargs=list(divisor='d'), rmin=0.01, q=1/2)$fitted$fixedpar$alpha, mc.cores=nb_core)))
+
+#MCE with K
+T_K = unlist(unname(mclapply(Simu[1:n], function(x) dppm(x~1, dppMatern(nu=2), method="mincon", statistic="K", rmin=0.01, q=1/2)$fitted$fixedpar$alpha, mc.cores=nb_core)))
+
+#Setting up the progress bar
+pb = txtProgressBar(max = n, style = 3)
+progress = function(n) setTxtProgressBar(pb, n)
+opts = list(progress = progress)
+
+#MLE (Without tore correction)
+cl = makeCluster(nb_core)
+clusterExport(cl, c('pairdist', 'is.ppp', 'area', 'dppMatern'))
+registerDoSNOW(cl)
+templist = foreach(a=1:n, .combine="cbind", .options.snow = opts) %dopar% {
+  MLEDPP(Simu[[a]], "WM", edgecorr=FALSE, sigma=2)$fixedpar$alpha}
+stopCluster(cl)
+T_MLE = unname(templist[1,])
+
+#MLE (With tore correction)
+cl = makeCluster(nb_core)
+clusterExport(cl, c('pairdist', 'is.ppp', 'area', 'dppMatern'))
+registerDoSNOW(cl)
+templist = foreach(a=1:n, .combine="cbind", .options.snow = opts) %dopar% {
+  MLEDPP(Simu[[a]], "WM", edgecorr=TRUE, sigma=2)$fixedpar$alpha}
+stopCluster(cl)
+T_MLE_corr = unname(templist[1,])
+
+#Boxplot of all statistics
+boxplot(T_MLE_corr-alpha0, T_MLE-alpha0, T_pcf-alpha0, T_K-alpha0, names=c("MLE w/ edge corr.", "MLE w/o edge corr.", "MCE w/ the pcf", "MCE w/ K"))
+abline(h=0, col="red", lty=2)
+#Mean square errors
+c(10^4*mean((T_MLE_corr-alpha0)^2), 10^4*mean((T_MLE-alpha0)^2), 10^4*mean((T_pcf-alpha0)^2), 10^4*mean((T_K-alpha0)^2))
+
+#Saving data
+all_data = data.frame(MLE_not_corrected = T_MLE, MLE_corrected = T_MLE_corr , MCE_with_g = T_pcf , MCE_with_K = T_K)
+saveRDS(all_data ,paste("results/Result_Matern_", toString(alpha0*1000), "e-3_[0_", toString(S_length), "].dat", sep=""))
+
+#####On an R-shaped window#####
+rho0 = 100
+alpha0 = 0.05
+S_length = 1
+nb_core = 7
+n = 100
+Simu = readRDS(paste("datasets/100_Gauss_", toString(alpha0*100), "e-2_letterR.dat", sep=""))
+
+#MCE with the pcf
+T_pcf = unlist(unname(mclapply(Simu[1:n], function(x) dppm(x~1, dppGauss, method="mincon", statistic="pcf", statargs=list(divisor='d'), rmin=0.01, q=1/2)$fitted$fixedpar$alpha, mc.cores=nb_core)))
+
+#MCE with K
+T_K = unlist(unname(mclapply(Simu[1:n], function(x) dppm(x~1, dppGauss, method="mincon", statistic="K", rmin=0.01, q=1/2)$fitted$fixedpar$alpha, mc.cores=nb_core)))
+
+#Setting up the progress bar
+pb = txtProgressBar(max = n, style = 3)
+progress = function(n) setTxtProgressBar(pb, n)
+opts = list(progress = progress)
+
+#MLE (Without tore correction)
+cl = makeCluster(nb_core)
+clusterExport(cl, c('pairdist', 'is.ppp', 'area', 'dppGauss'))
+registerDoSNOW(cl)
+templist = foreach(a=1:n, .combine="cbind", .options.snow = opts) %dopar% {
+  MLEDPP(Simu[[a]], "Gauss", edgecorr=FALSE)$fixedpar$alpha}
+stopCluster(cl)
+T_MLE = unname(templist[1,])
+
+#MLE (With tore correction)
+cl = makeCluster(nb_core)
+clusterExport(cl, c('pairdist', 'is.ppp', 'area', 'dppGauss', 'bdist.points'))
+registerDoSNOW(cl)
+templist = foreach(a=1:n, .combine="cbind", .options.snow = opts) %dopar% {
+  MLEDPP(Simu[[a]], "Gauss", edgecorr=TRUE)$fixedpar$alpha}
+stopCluster(cl)
+T_MLE_corr = unname(templist[1,])
+
+#Boxplot of all statistics
+boxplot(T_MLE_corr-alpha0, T_MLE-alpha0, T_pcf-alpha0, T_K-alpha0, names=c("MLE w/ edge corr.", "MLE w/o edge corr.", "MCE w/ the pcf", "MCE w/ K"))
+abline(h=0, col="red", lty=2)
+#Mean square errors
+c(10^4*mean((T_MLE_corr-alpha0)^2), 10^4*mean((T_MLE-alpha0)^2), 10^4*mean((T_pcf-alpha0)^2), 10^4*mean((T_K-alpha0)^2))
+
+##############################################Plotting all results##########################################################
+
+#Gauss
+par(mfrow=c(3,3), mar=c(1, 2.5, 2, 1))
+cmain = 2.5
+caxis = 1.5
+lwd = 1.5
+for (wind in c(1,2,3)){
+  for (alpha in c(1,3,5)){
+    titletext = bquote(alpha*"*="*.(alpha/100)*", W=[0,"*.(wind)*"]²"~"")
+    T = readRDS(paste("results/Result_Gauss_", alpha, "e-2_[0_", wind,"].dat", sep=""))
+    boxplot(T-alpha/100,yaxt="n", cex.main=cmain, main=titletext, xaxt="n", lwd=lwd)
+    cat('--------------------------------------------------------\n')
+    cat(paste("Mean square Errror (x10^4) for alpha=", alpha/100, " and window=[0,", wind, "]^2\n", sep=""))
+    print(10^4 * colMeans((T-alpha/100)^2))
+    abline(h=0, col="red", lty=2, lwd=thiccc)
+    axis(2, cex.axis=caxis)}}
+
+#Bessel
+par(mfrow=c(3,3), mar=c(1, 2.5, 2, 1))
+cmain = 2.5
+caxis = 1.5
+lwd = 1.5
+for (wind in c(1,2,3)){
+  for (alpha in c(1,3,5)){
+    titletext = bquote(alpha*"*="*.(alpha/100)*", W=[0,"*.(wind)*"]²"~"")
+    T = readRDS(paste("results/Result_Bessel_", alpha, "e-2_[0_", wind,"].dat", sep=""))
+    boxplot(T-alpha/100,yaxt="n", cex.main=cmain, main=titletext, xaxt="n", lwd=lwd)
+    cat('--------------------------------------------------------\n')
+    cat(paste("Mean square Errror (x10^4) for alpha=", alpha/100, " and window=[0,", wind, "]^2\n", sep=""))
+    print(10^4 * colMeans((T-alpha/100)^2))
+    abline(h=0, col="red", lty=2, lwd=thiccc)
+    axis(2, cex.axis=caxis)}}
+
+#Cauchy
+par(mfrow=c(3,3), mar=c(1, 2.5, 2, 1))
+cmain = 2.5
+caxis = 1.5
+lwd = 1.5
+for (wind in c(1,2,3)){
+  for (alpha in c(5,20,35)){
+    titletext = bquote(alpha*"*="*.(alpha/1000)*", W=[0,"*.(wind)*"]²"~"")
+    T = readRDS(paste("results/Result_Cauchy_", alpha, "e-3_[0_", wind,"].dat", sep=""))
+    boxplot(T-alpha/1000,yaxt="n", cex.main=cmain, main=titletext, xaxt="n", lwd=lwd)
+    cat('--------------------------------------------------------\n')
+    cat(paste("Mean square Errror (x10^4) for alpha=", alpha/1000, " and window=[0,", wind, "]^2\n", sep=""))
+    print(10^4 * colMeans((T-alpha/1000)^2))
+    abline(h=0, col="red", lty=2, lwd=thiccc)
+    axis(2, cex.axis=caxis)}}
+
+#Whittle-Matérn
+par(mfrow=c(1,2), mar=c(1, 2.5, 2, 1))
+cmain = 2.5
+caxis = 1.5
+lwd = 1.5
+alpha = 15
+for (wind in c(1,2)){
+  titletext = bquote(alpha*"*="*.(alpha/1000)*", W=[0,"*.(wind)*"]²"~"")
+  T = readRDS(paste("results/Result_Matern_", alpha, "e-3_[0_", wind,"].dat", sep=""))
+  boxplot(T-alpha/1000,yaxt="n", cex.main=cmain, main=titletext, xaxt="n", lwd=lwd)
+  cat('--------------------------------------------------------\n')
+  cat(paste("Mean square Errror (x10^4) for alpha=", alpha/1000, " and window=[0,", wind, "]^2\n", sep=""))
+  print(10^4 * colMeans((T-alpha/1000)^2))
+  abline(h=0, col="red", lty=2, lwd=thiccc)
+  axis(2, cex.axis=caxis)}
---
+++ MLE_DPP.R
@@ -0,0 +1,205 @@
+library(spatstat)
+library(stats)
+
+###############################General MLE function################################
+
+MLEDPP = function(ppp, DPPfamily, startpar=NULL, sigma=NULL, edgecorr=FALSE, Trunc=50){
+  if (!is.ppp(ppp)){
+    stop('First argument is not of the ppp type.')}
+  if (!(DPPfamily %in% c("Gauss", "Bessel", "Cauchy", "Whittle-Matern", "WM"))){
+    stop('Second argument should be either Gauss, Bessel, Cauchy or Whittle-Matern.')}
+  if ((DPPfamily == "WM" | DPPfamily == "Whittle-Matern") & is.null(sigma)){
+    stop('The sigma parameter has to be specified for the Whittle-Matern family')}
+  if(!is.null(sigma) & DPPfamily %in% c("Gauss", "Bessel", "Cauchy")){stop('The sigma parameter can only be specified for the Whittle-Matern family')}
+  
+  Win = ppp$window #Observation window
+  vol = area(Win) #Area of the observation window
+  
+  
+  
+  #Estimation of the intensity
+  rho_est = ppp$n/vol
+    
+  #Generating the distance matrix
+  if (!edgecorr){
+    M=as.matrix(dist(t(rbind(ppp$x, ppp$y)), method = "euclidean", diag = TRUE, upper = TRUE, p = 2))}
+  else {
+    if (Win$type == "rectangle") {M=pairdist(ppp, periodic=TRUE)}
+    else {  
+      warning("Edge correction is experimental for non rectangular windows.")
+      bdist=bdist.points(ppp)
+      M=as.matrix(dist(t(rbind(ppp$x, ppp$y)), method = "euclidean", diag = TRUE, upper = TRUE, p = 2)) #M is modified below, depending on the family
+    }
+  }
+  
+  #Setting up the log-likelihood function
+  if (DPPfamily == "Gauss"){
+    DPPfam = dppGauss(lambda=0, alpha=0, d=2) #DPP family
+    alphamax = sqrt(1/(pi*rho_est)) #Max value of alpha
+    
+    #Function corresponding to L_0^{rho,alpha}(r). Nsum is the truncation parameter.
+    Lker = function(rho, alpha, r, Nsum){
+      d = dim(r)[1]
+      bigM = array(rep(0, d*d), dim=c(d,d))
+      for (i in 1:Nsum){
+        bigM = bigM + (rho*pi*alpha^2)^(i-1)*exp(-r^2/(alpha^2*i))/i
+      }
+      return (rho * bigM)
+    }
+    
+    
+    if(edgecorr & Win$type != "rectangle"){
+      Nmax = Lker(rho_est, alphamax, M, Nsum=Trunc)
+      M = newM.nonrectangular_edgecorrection(Nmax, M, bdist)
+    }
+      
+     
+    
+    #Approximate log-likelihood of Gauss-type DPPs
+    Log_Likelihood = function(rho,alpha,M,vol,Nsum){
+      if (rho*pi*alpha^2 >= 1) {return(NA)}
+      N = Lker(rho,alpha,M,Nsum)
+      temp = determinant(N,logarithm=TRUE)
+      if(temp$sign == -1){return(NA)} #Returns NA if the determinant in the stochastic part of the log-likelihood is negative.
+      a = integrate(function(r) 2*pi*r*log(1-rho*pi*alpha^2*exp(-(pi*alpha*r)^2)),0,Inf)$value+(1/vol)*temp$modulus[1]
+      return(-a)} #The opposite of the log-likelihood is returned since the optimize function looks for minima.
+    }
+  
+  if (DPPfamily == "Cauchy"){
+    DPPfam = dppCauchy(lambda=0, alpha=0, nu=0.5, d=2) #DPP family
+    alphamax = sqrt(1/(2*pi*rho_est)) #Max value of alpha
+    
+    #Function corresponding to L_0^{rho,alpha}(r). Nsum is the truncation parameter.
+    Lker = function(rho, alpha, r, Nsum){
+      d = dim(r)[1]
+      bigM = array(rep(0, d*d), dim=c(d,d))
+      for (i in 1:Nsum){
+        bigM = bigM + (2*rho*pi*alpha^2)^(i-1)/(i^2*(1+(r/(alpha*i))^2)^(3/2))
+      }
+      return (rho * bigM)
+    }
+    
+    if(edgecorr & Win$type != "rectangle"){
+      Nmax = Lker(rho_est, alphamax, M, Nsum=Trunc)
+      M = newM.nonrectangular_edgecorrection(Nmax, M, bdist)
+    }
+    
+    
+    #Approximate log-likelihood of Cauchy-type DPPs
+    Log_Likelihood = function(rho, alpha, M, vol, Nsum){
+      if (2*rho*pi*alpha^2 >= 1) {return(NA)}
+      N = Lker(rho, alpha, M, Nsum)
+      temp = determinant(N,logarithm=TRUE)
+      if(temp$sign == -1){return(NA)} #Returns NA if the determinant in the stochastic part of the log-likelihood is negative.
+      a=-rho*sum((2*rho*pi*alpha^2)^(1:Nsum-1)/(1:Nsum)^3)+(1/vol)*temp$modulus[1]
+      return(-a) #The opposite of the log-likelihood is returned since the optimize function looks for minima.
+      }
+    }
+  
+  if (DPPfamily == "Bessel"){
+    DPPfam = dppBessel(lambda=0, alpha=0, sigma=0, d=2) #DPP family
+    alphamax = sqrt(1/(pi*rho_est)) #Max value of alpha
+    
+    if(edgecorr & Win$type != "rectangle"){
+      Nmax = (besselJ(2*M/alphamax,1)/(M/alphamax))*rho_est/(1-rho_est*pi*alphamax^2)
+      for (i in 1:sqrt(length(M))){
+        Nmax[i,i] = rho_est/(1-rho_est*pi*alphamax^2)
+      }
+      M = newM.nonrectangular_edgecorrection(Nmax,M,bdist)
+    }
+    
+    
+    #Approximate log-likelihood of Bessel-type DPPs
+    Log_Likelihood = function(rho,alpha,M,vol,Nsum=NULL){
+      if (rho*pi*alpha^2 >= 1) {return(NA)}
+      N = (besselJ(2*M/alpha,1)/(M/alpha))*rho/(1-rho*pi*alpha^2)
+      for (i in 1:sqrt(length(M))){
+        N[i,i] = rho/(1-rho*pi*alpha^2)
+      }
+      temp = determinant(N,logarithm=TRUE)
+      if(temp$sign == -1){return(NA)} #Returns NA if the determinant in the stochastic part of the log-likelihood is negative.
+      a = log(1-rho*pi*alpha^2)/(pi*alpha^2)+(1/vol)*temp$modulus[1]
+      return(-a) #The opposite of the log-likelihood is returned since the optimize function looks for minima.
+      }
+    }
+  
+  if (DPPfamily == "WM" | DPPfamily == "Whittle-Matern"){
+    DPPfam = dppMatern(lambda=0, alpha=0, nu=sigma, d=2) #DPP family
+    alphamax = sqrt(1/(pi*rho_est*4*sigma)) #Max value of alpha
+    
+    #Function corresponding to L_0^{rho,alpha}(r). Nsum is the truncation parameter.
+    Lker = function(rho,alpha,r,Nsum){
+      d = dim(r)[1]
+      bigM = array(rep(0, d*d), dim=c(d,d))
+      for (i in 1:Nsum){
+        bigM = bigM + (4*rho*pi*alpha^2*sigma)^(i-1)/gamma(i*(sigma+1))*exp((i*(sigma+1)-1)*log(r/(2*alpha))-r/alpha)*besselK(r/alpha,(sigma+1)*i-1,expon.scaled = TRUE)
+      }
+      return (2 * rho * sigma * bigM)
+    }
+    
+    if(edgecorr & Win$type != "rectangle"){
+      Nmax = Lker(rho_est,alphamax,M,Nsum=Trunc)
+      M = newM.nonrectangular_edgecorrection(Nmax,M,bdist)
+    }
+    
+    
+    #Approximate log-likelihood of Whittle-Matern-type DPPs
+    Log_Likelihood = function(rho, alpha, M, vol, Nsum=NULL){
+      if (4*rho*pi*sigma*alpha^2 >= 1) {return(NA)}
+      N = Lker(rho, alpha, M, Nsum)
+      diagN = rho*sigma*sum((4*rho*pi*alpha^2*sigma)^(1:Nsum-1)/((sigma+1)*(1:Nsum)-1))
+      diag(N) = diagN
+      temp = determinant(N, logarithm=TRUE)
+      if(temp$sign == -1){return(NA)} #Returns NA if the determinant in the stochastic part of the log-likelihood is negative.
+      else{
+        a = integrate(function(r) 2*pi*r*log(1-rho*sigma*4*pi*alpha^2/(1+4*pi^2*alpha^2*r^2)^(sigma+1)),0,Inf)$value+(1/vol)*temp$modulus[1]
+        return(-a) #The opposite of the log-likelihood is returned since the optimize function looks for minima.
+        }
+      }
+    }
+  
+  if (is.null(startpar)){startpar = alphamax/2} #By default, the starting parameter is half the maximum possible value  of alpha
+  if (startpar > alphamax | startpar < 0){stop("Starting paramater not in range")}
+  
+  #Estimation of alpha by the maximum of the approximate log-likelihood
+  alpha_est = optim(par=startpar, fn=Log_Likelihood, M=M,vol=vol,Nsum=Trunc, rho=rho_est, lower=alphamax/1000, upper=alphamax, method="Brent")$par
+  
+  DPPfam$fixedpar$lambda = rho_est
+  DPPfam$fixedpar$alpha = alpha_est
+  
+  return(DPPfam)
+}
+
+
+newM.nonrectangular_edgecorrection = function(Nmax, M, bdist){
+  l = dim(M)[1]
+  dim(Nmax) = c(l,l)
+  tempbool = Nmax > 0.001*max(Nmax)
+  rmax = max(M[tempbool])
+  
+  noborder = bdist>rmax
+  if(sum(noborder) == 0) noborder = which.max(bdist)
+  
+  distpossible = M[noborder,]
+  distpossible = matrix(distpossible, ncol=l)
+  
+  nb.replace.possible = apply(distpossible < rmax, 1, sum)
+  
+  index = order(bdist)
+  indexmax = which(sort(bdist) > rmax)
+  if(length(indexmax) == 0){imax=l}else{imax = min(indexmax)}
+  
+  Mnew = M
+  for(i in 1:(imax-1)){
+    distpossible.i = distpossible[distpossible > bdist[index[i]]]
+    nb.replace.i = sample(nb.replace.possible, 1) - sum(M[index[i],] < rmax)
+    if(nb.replace.i > 0){
+      for(j in (i+1):min(l,i+nb.replace.i)){
+        if(Mnew[index[i], index[j]] > rmax){
+          Mnew[index[i], index[j]] = sample(distpossible.i, 1)
+          Mnew[index[j], index[i]] = Mnew[index[i], index[j]]
+        }
+      }}
+  }
+  return(Mnew)
+}
