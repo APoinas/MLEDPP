8c16b30c9f60e96c087b0420d5b0b2a11cad3135
APoinas <arnaud.poinas@univ-lille.fr>
2021-05-18 15:34:08 +0200

    Uniformized the parameters of the derivatives of the log-likelihood

--- LL_derivatives/Cauchy.R
+++ LL_derivatives/Cauchy.R
@@ -1,95 +1,95 @@
 #L0
-L0=function(rho,alpha,r,Nsum){
-  d = dim(r)[1]
+L0=function(rho, alpha, M, Nsum){
+  d = dim(M)[1]
   bigM = array(rep(0, d*d), dim=c(d,d))
   for (i in 1:Nsum){
-    bigM = bigM + (2*rho*pi*alpha^2)^(i-1)/(i^2*(1+(r/(alpha*i))^2)^(3/2))
+    bigM = bigM + (2*rho*pi*alpha^2)^(i-1)/(i^2*(1+(M/(alpha*i))^2)^(3/2))
   }
   return (rho * bigM)
 }
 
 #Derivative of L0 with respect to alpha
-DalpL0=function(rho,alpha,r,Nsum){
-  d = dim(r)[1]
+DalpL0=function(rho, alpha, M, Nsum){
+  d = dim(M)[1]
   bigM = array(rep(0, d*d), dim=c(d,d))
   for (i in 1:Nsum){
-    bigM = bigM + (2*rho*pi)^(i-1) * alpha^(2*i-3) * (2*i+1-3/(1 + (r / (i * alpha))^2))    /(i^2*(1+(r/(alpha*i))^2)^(3/2))
+    bigM = bigM + (2*rho*pi)^(i-1) * alpha^(2*i-3) * (2*i+1-3/(1 + (M / (i * alpha))^2))    /(i^2*(1+(M/(alpha*i))^2)^(3/2))
   }
   return (rho * bigM)
 }
 
 #2nd derivative of L0 with respect to alpha
-D2alpL0=function(rho,alpha,r,Nsum){
-  d = dim(r)[1]
+D2alpL0=function(rho, alpha, M, Nsum){
+  d = dim(M)[1]
   bigM = array(rep(0, d*d), dim=c(d,d))
   for (i in 1:Nsum){
-    bigM = bigM + ((2*rho*pi)^(i-1)/(i^2)) * ( ( (2*i-2) * alpha^(2*i-4) * (2*i-3/(1 + (r / (i * alpha))^2))    /((1+(r/(alpha*i))^2)^(3/2)) ) + ( 3*(r/i)^2 * alpha^(2*i-6) * (2*i-5/(1 + (r / (i * alpha))^2))    /((1+(r/(alpha*i))^2)^(5/2)) ) )
+    bigM = bigM + ((2*rho*pi)^(i-1)/(i^2)) * ( ( (2*i-2) * alpha^(2*i-4) * (2*i-3/(1 + (M / (i * alpha))^2))    /((1+(M/(alpha*i))^2)^(3/2)) ) + ( 3*(M/i)^2 * alpha^(2*i-6) * (2*i-5/(1 + (M / (i * alpha))^2))    /((1+(M/(alpha*i))^2)^(5/2)) ) )
   }
   return (rho * bigM)
 }
 
 #Derivative of L0 with respect to rho
-DrhoL0=function(rho,alpha,r,Nsum){
-  d = dim(r)[1]
+DrhoL0=function(rho, alpha, M, Nsum){
+  d = dim(M)[1]
   bigM = array(rep(0, d*d), dim=c(d,d))
   for (i in 1:Nsum){
-    bigM = bigM + (2*rho*pi*alpha^2)^(i-1)/(i*(1+(r/(alpha*i))^2)^(3/2))
+    bigM = bigM + (2*rho*pi*alpha^2)^(i-1)/(i*(1+(M/(alpha*i))^2)^(3/2))
   }
   return (bigM)
 }
 
 #2nd derivative of L0 with respect to rho
-D2rhoL0=function(rho,alpha,r,Nsum){
-  d = dim(r)[1]
+D2rhoL0=function(rho, alpha, M, Nsum){
+  d = dim(M)[1]
   bigM = array(rep(0, d*d), dim=c(d,d))
   for (i in 1:Nsum){
-    bigM = bigM + (2*pi*alpha^2)^(i-1) * rho^(i-2) * (i-1)/(i*(1+(r/(alpha*i))^2)^(3/2))
+    bigM = bigM + (2*pi*alpha^2)^(i-1) * rho^(i-2) * (i-1)/(i*(1+(M/(alpha*i))^2)^(3/2))
   }
   return (bigM)
 }
 
 #Derivative of L0 with respect to alpha and rho
-DalpDrhoL0=function(rho,alpha,r,Nsum){
-  d = dim(r)[1]
+DalpDrhoL0=function(rho, alpha, M, Nsum){
+  d = dim(M)[1]
   bigM = array(rep(0, d*d), dim=c(d,d))
   for (i in 1:Nsum){
-    bigM = bigM + (2*rho*pi)^(i-1) * alpha^(2*i-3) * (2*i-2+3/(1 + i^2 * alpha^2 / r^2))    /(i*(1+(r/(alpha*i))^2)^(3/2))
+    bigM = bigM + (2*rho*pi)^(i-1) * alpha^(2*i-3) * (2*i-2+3/(1 + i^2 * alpha^2 / M^2))    /(i*(1+(M/(alpha*i))^2)^(3/2))
   }
   return (bigM)
 }
 
 #Integral part of the log-likelihood
-Integ = function(rho,alpha,Nsum){
+Integ = function(rho, alpha, Nsum){
   N = 1:Nsum
   return(-rho * sum( (2*rho*pi*alpha^2)^(N-1) / (N^3)))
 }
 
 #Derivative of the integral part of the log-likelihood with respect to alpha
-DalpInteg = function(rho,alpha,Nsum){
+DalpInteg = function(rho, alpha, Nsum){
   N = 1:Nsum
   return(- 2 * rho * sum( (N-1) * (2*rho*pi)^(N-1) * alpha^(2*N-3) / (N^3)))
 }
 
 #2nd derivative of the integral part of the log-likelihood with respect to alpha
-D2alpInteg = function(rho,alpha,Nsum){
+D2alpInteg = function(rho, alpha, Nsum){
   N = 1:Nsum
   return(- 2 * rho * sum( (N-1) * (2*N-3) * (2*rho*pi)^(N-1) * alpha^(2*N-4) / (N^3)))
 }
 
 #Derivative of the integral part of the log-likelihood with respect to rho
-DrhoInteg = function(rho,alpha,Nsum){
+DrhoInteg = function(rho, alpha, Nsum){
   N = 1:Nsum
   return(- sum( (2*rho*pi*alpha^2)^(N-1) / (N^2)))
 }
 
 #2nd derivative of the integral part of the log-likelihood with respect to rho
-D2rhoInteg = function(rho,alpha,Nsum){
+D2rhoInteg = function(rho, alpha, Nsum){
   N = 1:Nsum
   return(- sum( (N-1) * (2*pi*alpha^2)^(N-1) * rho^(N-2) / (N^2)))
 }
 
 #Derivative of the integral part of the log-likelihood with respect to rho and alpha
-DalpDrhoInteg = function(rho,alpha,Nsum){
+DalpDrhoInteg = function(rho, alpha, Nsum){
   N = 1:Nsum
   return(- 2 * sum( (N-1) * (2*rho*pi)^(N-1) * alpha^(2*N-3) / (N^2)))
 }
--- LL_derivatives/Gauss.R
+++ LL_derivatives/Gauss.R
@@ -1,105 +1,93 @@
 #L0
-L0=function(rho,alpha,r,Nsum){
-  N = 1:Nsum
-  if (is.null(dim(r))){return(rho*sum((rho*pi*alpha^2)^(N-1)*exp(-r^2/(alpha^2*N))/N))}
-  d = dim(r)[1]
+L0=function(rho, alpha, M, Nsum){
+  d = dim(M)[1]
   bigM = array(rep(0, d*d), dim=c(d,d))
-  for (i in N){
-    bigM = bigM + (rho*pi*alpha^2)^(i-1)*exp(-r^2/(alpha^2*i))/i
+  for (i in 1:Nsum){
+    bigM = bigM + (rho*pi*alpha^2)^(i-1)*exp(-M^2/(alpha^2*i))/i
   }
   return (rho * bigM)
 }
 
 #Derivative of L0 with respect to alpha
-DalpL0=function(rho,alpha,r,Nsum){
-  N = 1:Nsum
-  if (is.null(dim(r))){return(2*rho*sum((rho*pi)^(N-1) * ((N-1) * alpha^(2*N-3) + r^2 * alpha^(2*N-5)/N) * exp(-r^2/(alpha^2*N))/N))}
-  d = dim(r)[1]
+DalpL0=function(rho, alpha, M, Nsum){
+  d = dim(M)[1]
   bigM = array(rep(0, d*d), dim=c(d,d))
-  for (i in N){
-    bigM = bigM + (rho*pi)^(i-1)*((i-1) * alpha^(2*i-3) + r^2 * alpha^(2*i-5)/i)*exp(-r^2/(alpha^2*i))/i
+  for (i in 1:Nsum){
+    bigM = bigM + (rho*pi)^(i-1)*((i-1) * alpha^(2*i-3) + M^2 * alpha^(2*i-5)/i)*exp(-M^2/(alpha^2*i))/i
   }
   return (2 * rho * bigM)
 }
 
 #2nd derivative of L0 with respect to alpha
-D2alpL0=function(rho,alpha,r,Nsum){
-  N = 1:Nsum
-  if (is.null(dim(r))){return(2*rho*sum((rho*pi)^(N-1) * ((N-1) * (2*N-3) * alpha^(2*N-4) + r^2 * (4*N-7) * alpha^(2*N-6)/N + 2*r^4 * alpha^(2*N-8)/ (N^2)) * exp(-r^2/(alpha^2*N))/N))}
-  d = dim(r)[1]
+D2alpL0=function(rho, alpha, M, Nsum){
+  d = dim(M)[1]
   bigM = array(rep(0, d*d), dim=c(d,d))
-  for (i in N){
-    bigM = bigM + (rho*pi)^(i-1) * ((i-1) * (2*i-3) * alpha^(2*i-4) + r^2 * (4*i-7) * alpha^(2*i-6)/i + 2*r^4 * alpha^(2*i-8)/ (i^2))*exp(-r^2/(alpha^2*i))/i
+  for (i in 1:Nsum){
+    bigM = bigM + (rho*pi)^(i-1) * ((i-1) * (2*i-3) * alpha^(2*i-4) + M^2 * (4*i-7) * alpha^(2*i-6)/i + 2*M^4 * alpha^(2*i-8)/ (i^2))*exp(-M^2/(alpha^2*i))/i
   }
   return (2 * rho * bigM)
 }
 
 #Derivative of L0 with respect to rho
-DrhoL0=function(rho,alpha,r,Nsum){
-  N = 1:Nsum
-  if (is.null(dim(r))){return(sum((rho*pi*alpha^2)^(N-1)*exp(-r^2/(alpha^2*N))))}
-  d = dim(r)[1]
+DrhoL0=function(rho, alpha, M, Nsum){
+  d = dim(M)[1]
   bigM = array(rep(0, d*d), dim=c(d,d))
-  for (i in N){
-    bigM = bigM + (rho*pi*alpha^2)^(i-1)*exp(-r^2/(alpha^2*i))
+  for (i in 1:Nsum){
+    bigM = bigM + (rho*pi*alpha^2)^(i-1)*exp(-M^2/(alpha^2*i))
   }
   return (bigM)
 }
 
 #2nd derivative of L0 with respect to rho
-D2rhoL0=function(rho,alpha,r,Nsum){
-  N = 1:Nsum
-  if (is.null(dim(r))){return(sum(rho^(N-2) * (pi*alpha^2)^(N-1) * (N-1) * exp(-r^2/(alpha^2*N))))}
-  d = dim(r)[1]
+D2rhoL0=function(rho, alpha, M, Nsum){
+  d = dim(M)[1]
   bigM = array(rep(0, d*d), dim=c(d,d))
-  for (i in N){
-    bigM = bigM + rho^(i-2) * (pi*alpha^2)^(i-1) * (i-1) * exp(-r^2/(alpha^2*i))
+  for (i in 1:Nsum){
+    bigM = bigM + rho^(i-2) * (pi*alpha^2)^(i-1) * (i-1) * exp(-M^2/(alpha^2*i))
   }
   return (bigM)
 }
 
 #Derivative of L0 with respect to alpha and rho
-DalpDrhoL0=function(rho,alpha,r,Nsum){
-  N = 1:Nsum
-  if (is.null(dim(r))){return(2*sum((rho*pi)^(N-1) * ((N-1) * alpha^(2*N-3) + r^2 * alpha^(2*N-5)/N) * exp(-r^2/(alpha^2*N))))}
-  d = dim(r)[1]
+DalpDrhoL0=function(rho, alpha, M, Nsum){
+  d = dim(M)[1]
   bigM = array(rep(0, d*d), dim=c(d,d))
-  for (i in N){
-    bigM = bigM + (rho*pi)^(i-1)*((i-1) * alpha^(2*i-3) + r^2 * alpha^(2*i-5)/i)*exp(-r^2/(alpha^2*i))
+  for (i in 1:Nsum){
+    bigM = bigM + (rho*pi)^(i-1)*((i-1) * alpha^(2*i-3) + M^2 * alpha^(2*i-5)/i)*exp(-M^2/(alpha^2*i))
   }
   return (2 * bigM)
 }
 
 #Integral part of the log-likelihood
-Integ = function(rho,alpha,Nsum){
+Integ = function(rho, alpha, Nsum){
   N = 1:Nsum
   return(-rho * sum( (rho*pi*alpha^2)^(N-1) / (N^2)))
 }
 
 #Derivative of the integral part of the log-likelihood with respect to alpha
-DalpInteg = function(rho,alpha,Nsum){
+DalpInteg = function(rho, alpha, Nsum){
   N = 1:Nsum
   return(-rho * sum( (rho*pi)^(N-1) * alpha^(2*N - 3) * (2*N - 2) / (N^2)))
 }
 
 #2nd derivative of the integral part of the log-likelihood with respect to alpha
-D2alpInteg = function(rho,alpha,Nsum){
+D2alpInteg = function(rho, alpha, Nsum){
   N = 1:Nsum
   return(-rho * sum( (rho*pi)^(N-1) * alpha^(2*N - 4) * (2*N - 3) * (2*N - 2) / (N^2)))
 }
 
 #Derivative of the integral part of the log-likelihood with respect to rho
-DrhoInteg = function(rho,alpha,Nsum){
+DrhoInteg = function(rho, alpha, Nsum=NULL){
   return(log(1-rho*pi*alpha^2)/(rho*pi*alpha^2))
 }
 
 #2nd derivative of the integral part of the log-likelihood with respect to rho
-D2rhoInteg = function(rho,alpha,Nsum){
+D2rhoInteg = function(rho, alpha, Nsum=NULL){
   return(-(1/(1-rho*pi*alpha^2) + log(1-rho*pi*alpha^2)/(rho*pi*alpha^2))/rho)
 }
 
 #Derivative of the integral part of the log-likelihood with respect to rho and alpha
-DalpDrhoInteg = function(rho,alpha,Nsum){
+DalpDrhoInteg = function(rho, alpha, Nsum){
   N = 1:Nsum
   return(-2 * sum( (rho*pi)^(N-1) * alpha^(2*N - 3) * (N - 1) / N))
 }
